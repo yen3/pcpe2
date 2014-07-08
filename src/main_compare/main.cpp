6318a0d506da7031b0fe269d8728628ecd76e255
yen3 <yen3rc@gmail.com>
2014-07-08 22:14:03 +0800

    add hash table compare

---
+++ src/main_compare/main.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+const std::size_t HASH_TABLE_SIZE= 100003;
+const std::size_t SUBSTRING_SIZE = 6;
+
+typedef std::vector<std::string> SeqList;
+
+/* 
+ * key:    hash value
+ * value:  <substring (6 chars), <sequence index, substring location index> >
+ * */
+typedef std::vector<std::map<std::string, std::vector<std::pair<int,int> > > > HashTable;
+
+
+/**
+ * @brief read sequence list from formated fasta file 
+ *
+ * @param[out] seq      sequence list
+ * @param[in]  filename input filename
+ */
+void read_sequence_list(SeqList& seq, const char* filename)
+{
+    std::ifstream in_file(filename, std::ifstream::in);
+
+    std::size_t read_size=0, str_read_size=0;
+    std::string s;
+    in_file >> str_read_size;
+    for(std::size_t i=0; i<str_read_size; i++){
+        in_file >> read_size >> s;
+        seq.push_back(s);
+    }
+
+    in_file.close();
+    in_file.clear();
+}
+
+inline std::size_t hash_value(const char* s)
+{
+    return (s[0]-'A')*11881376 + (s[1]-'A')*456976 + (s[2]-'A')*17576 + (s[3]-'A')*676 + (s[4]-'A')*26 + (s[5]-'A')*1;
+}
+
+void add_substring_to_hashtable(const std::size_t seq_index, const std::string& s, HashTable& ht)
+{
+    if(s.size() < SUBSTRING_SIZE) return;
+
+    for(std::size_t substr_index = 0;
+        substr_index < s.size()-SUBSTRING_SIZE;
+        ++substr_index){
+#if defined(__DEBUG__)
+        try{
+            ht[hash_value(s.c_str()+ substr_index) % ht.size()]
+                [s.substr(substr_index, SUBSTRING_SIZE)].push_back(std::make_pair(seq_index, substr_index));
+        }catch(const std::out_of_range& oor){
+            std::cerr << "Error " << substr_index << " " << s << " " << oor.what() << std::endl;
+            exit(1);
+        }
+#else 
+        ht[hash_value(s.c_str()+ substr_index) % ht.size()]
+            [s.substr(substr_index, SUBSTRING_SIZE)].push_back(
+                    std::make_pair(seq_index, substr_index));
+#endif /*  __DEBUG__ */
+    }
+}
+
+/**
+ * @brief create hash table from file 
+ *
+ * @param[out] ht       hash table
+ * @param[in]  filename input filename
+ *
+ */
+void create_hash_table(HashTable& ht, const char* filename)
+{
+    // read sequence list from file
+    SeqList seq_list;
+    read_sequence_list(seq_list, filename);
+
+    // add substrings information to hashtable
+    for(std::size_t seq_index=0; seq_index< seq_list.size(); ++seq_index){
+        add_substring_to_hashtable(seq_index, seq_list[seq_index], ht);
+    }
+}
+
+inline void output_to_file(std::ofstream& out_file,
+                           const std::string& mcs,
+                           const std::vector<std::pair<int, int>>& lx, 
+                           const std::vector<std::pair<int, int>>& ly)
+{
+    for_each(lx.begin(), lx.end(),
+        [&out_file, &mcs, &ly](const std::pair<int, int>& x){
+            for_each(ly.begin(), ly.end(), 
+                [&out_file, &mcs, &x](const std::pair<int, int>& y){
+                        out_file << x.first << " " << y.first << " "
+                                 << x.second << " " << y.second << " " 
+                                 << mcs << std::endl;
+            });
+    });
+}
+
+int compare_hash_table(const std::string& out_fn, const HashTable& x, const HashTable& y)
+{
+    if(x.size() != y.size()){
+        return -1;
+    }
+
+    std::ofstream out_file(out_fn, std::ofstream::out);
+
+    for(std::size_t m_index = 0; m_index < x.size(); ++m_index){
+        for(auto hx = x[m_index].begin(), hy = y[m_index].begin();
+                 hx != x[m_index].end() && hy != y[m_index].end();)
+        {
+            if(hx->first == hy->first){
+                output_to_file(out_file, hx->first, hx->second, hy->second);
+                ++hx;
+                ++hy;
+            }
+            else if(hx->first > hy->first)  ++hy; 
+            else   ++hx;
+        }
+    }
+
+    out_file.clear();
+    out_file.close();
+
+    return 0;
+}
+
+int main(int argc, char const* argv[])
+{
+
+    /**
+     *   argv[1]: input sequence a
+     *   argv[2]: input sequence b
+     *   argv[3]: output file
+     * */
+    std::string input_fn_a;
+    std::string input_fn_b;
+    std::string output_fn;
+
+    if(argc == 4){
+        input_fn_a = argv[1];
+        input_fn_b = argv[2];
+        output_fn = argv[3];
+    }
+
+#if defined(__DEBUG__)
+    std::cout << input_fn_a << std::endl << input_fn_b << std::endl << output_fn << std::endl;
+#endif /* __DEBUG__ */
+
+    HashTable hta(HASH_TABLE_SIZE);
+    create_hash_table(hta, input_fn_a.c_str());
+
+#if defined(__DEBUG__)
+    std::cout << "create hash table a finish." << std::endl;
+#endif /* __DEBUG__ */
+
+    HashTable htb(HASH_TABLE_SIZE);
+    create_hash_table(htb, input_fn_b.c_str());
+
+#if defined(__DEBUG__)
+    std::cout << "create hash table b finish." << std::endl;
+#endif /* __DEBUG__ */
+
+    compare_hash_table(output_fn, hta, htb);
+
+#if defined(__DEBUG__)
+    std::cout << "compare hash table a b finsish." << std::endl;
+#endif /* __DEBUG__ */
+
+    return 0;
+}
+
---
+++ src/read_fasta/read_fasta.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+getOutIDFilename = lambda fn: os.path.splitext(fn)[0] + "_id_css.txt"
+getOutSeqFilename = lambda fn: os.path.splitext(fn)[0] + "_seq_css.txt"
+extractID = lambda id_line: id_line.split('|')[1]
+
+
+def readEntry(fn):
+    id_line = ""
+    seq = ""
+    f = open(fn)
+    while True:
+        l = f.readline()
+        if l != "":
+            if l.startswith('>'):
+                if id_line != "" and seq != "":
+                    yield seq, id_line
+                id_line = l
+                seq = ""
+            else:
+                seq += l[:-1]
+        else:
+            break
+
+    if id_line and seq:
+        yield seq, id_line
+
+
+def readFastaSet(fn):
+    seq_map = dict()
+    for s, i in readEntry(fn):
+        seq_map.setdefault(s, []).append(extractID(i))
+
+    return seq_map
+
+
+def writeResult(fn, seq_map):
+    f_id = open(getOutIDFilename(fn), 'w')
+    f_seq = open(getOutSeqFilename(fn), 'w')
+
+    f_id.write(str(len(seq_map)) + "\n")
+    f_seq.write(str(len(seq_map)) + "\n")
+
+    for seq, id_list in seq_map.iteritems():
+        f_id.write(str(len(id_list)) + " " + " ".join(id_list) + "\n")
+        f_seq.write(str(len(seq)) + " " + seq + "\n")
+
+    f_id.close()
+    f_seq.close()
+
+
+def run(fn):
+    writeResult(fn, readFastaSet(fn))
+    print "Read file " + fn
+
+
+def main():
+    fn_list = sys.argv[1:] if len(sys.argv) > 1 else ['example/influenza.faa']
+
+    if len(fn_list) > 1:
+        pool = Pool(processes=cpu_count())
+        pool.map(run, fn_list)
+        pool.terminate()
+        pool.close()
+    else:
+        map(run, fn_list)
+
+if __name__ == "__main__":
+    main()
