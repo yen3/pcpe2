f0295671d8cd5821c0427b5c0c3f854f7c70f40a
yen3 <yen3rc@gmail.com>
2014-08-02 17:36:36 +0800

    finish esort (not tested)

--- src/main_compare/inc/pcpe_util.h
+++ src/main_compare/inc/pcpe_util.h
@@ -1,10 +1,77 @@
+#ifndef __PCPE_UTIL_H__
+#define __PCPE_UTIL_H__
+
+#include <fstream>
 #include <vector>
 #include <string>
+#include <memory>
 
 namespace pcpe{
 
 typedef std::string Filename;
 typedef std::vector<Filename> FilenameList;
 
+class LocationInfo;
+typedef std::shared_ptr<LocationInfo> LocationInfoPtr;
+
+std::istream& operator>>(std::istream& in, LocationInfo& li);
+std::ostream& operator<<(std::ostream& out, const LocationInfo& li); 
+
+class LocationInfo{
+    friend std::istream& operator>>(std::istream& in, LocationInfo& li);
+    friend std::ostream& operator<<(std::ostream& in, const LocationInfo& li);
+public:
+    LocationInfo():x_(0), y_(0), x_loc_(0), y_loc_(0)
+    {
+    }
+
+    bool operator<(const LocationInfo& rhs) const
+    {
+        int c[4] = {x_ - rhs.x_,
+                    y_ - rhs.y_,
+                    x_loc_ - rhs.x_loc_,
+                    y_loc_ - rhs.y_loc_};
+
+        for(std::size_t cidx= 0; cidx< 4; ++cidx){
+            if(c[cidx]  != 0){
+                return c[cidx] < 0;
+            } 
+        }
+        return false;
+    }
+
+    bool operator>(const LocationInfo& rhs) const
+    {
+        int c[4] = {x_ - rhs.x_,
+                    y_ - rhs.y_,
+                    x_loc_ - rhs.x_loc_,
+                    y_loc_ - rhs.y_loc_};
+
+        for(std::size_t cidx= 0; cidx< 4; ++cidx){
+            if(c[cidx]  != 0){
+                return c[cidx] > 0;
+            } 
+        }
+        return false;
+    }
+
+    bool operator==(const LocationInfo& rhs) const
+    {
+        return (x_ == rhs.x_) && (y_ == rhs.y_) &&
+               (x_loc_ == rhs.x_loc_) && (y_loc_ && rhs.y_loc_);
+    }
+    
+    bool operator>=(const LocationInfo& rhs) const { return !(*this < rhs); }
+    bool operator<=(const LocationInfo& rhs) const { return !(*this >= rhs); }
+    bool operator!=(const LocationInfo& rhs) const { return !(*this == rhs); }
+
+private:
+    int x_;
+    int y_;
+    int x_loc_;
+    int y_loc_;
+}; 
+
 } // namespace pcpe
 
+#endif
--- src/main_compare/src/esort.cpp
+++ src/main_compare/src/esort.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
 #include <fstream>
+#include <iterator>
 #include <vector>
 #include <string>
 #include <thread>
 #include <atomic>
 #include <array>
+#include <algorithm>
 
+#include "pcpe_util.h"
 #include "esort.h"
 
 namespace pcpe{
@@ -17,61 +21,22 @@ std::atomic_uint esfl_index(0);
 /*****************************************************************************/
 //  Classes
 /*****************************************************************************/
-class LocationInfo{
-public:
-    bool operator<(const LocationInfo& rhs) const
-    {
-        int c[4] = {x_ - rhs.x_,
-                    y_ - rhs.y_,
-                    x_loc_ - rhs.x_loc_,
-                    y_loc_ - rhs.y_loc_};
-
-        for(std::size_t cidx= 0; cidx< 4; ++cidx){
-            if(c[cidx]  != 0){
-                return c[cidx] < 0;
-            } 
-        }
-        return false;
-    }
-
-    bool operator>(const LocationInfo& rhs) const
-    {
-        int c[4] = {x_ - rhs.x_,
-                    y_ - rhs.y_,
-                    x_loc_ - rhs.x_loc_,
-                    y_loc_ - rhs.y_loc_};
-
-        for(std::size_t cidx= 0; cidx< 4; ++cidx){
-            if(c[cidx]  != 0){
-                return c[cidx] > 0;
-            } 
-        }
-        return false;
-    }
-
-    bool operator==(const LocationInfo& rhs) const
-    {
-        return (x_ == rhs.x_) && (y_ == rhs.y_) &&
-               (x_loc_ == rhs.x_loc_) && (y_loc_ && rhs.y_loc_);
-    }
-    
-    bool operator>=(const LocationInfo& rhs) const { return !(*this < rhs); }
-    bool operator<=(const LocationInfo& rhs) const { return !(*this >= rhs); }
-    bool operator!=(const LocationInfo& rhs) const { return !(*this == rhs); }
-
-private:
-    int x_;
-    int y_;
-    int x_loc_;
-    int y_loc_;
-}; 
-
 class LocationInfoFile{
-    static const std::size_t READ_MAX_ENTRY_SIZE = 10000;
+    static constexpr std::size_t READ_MAX_SIZE = 10000;
 public:
+    LocationInfoFile(const Filename& fn);
+    LocationInfoPtr getEntry();
+    bool empty();
+
+    bool operator<(const LocationInfoFile& rhs)  const { return *li_[0] < *rhs.li_[0];  }
+    bool operator>(const LocationInfoFile& rhs)  const { return *li_[0] > *rhs.li_[0];  }
+    bool operator==(const LocationInfoFile& rhs) const { return *li_[0] == *rhs.li_[0]; }
+    bool operator>=(const LocationInfoFile& rhs) const { return !(*this < rhs);         }
+    bool operator<=(const LocationInfoFile& rhs) const { return !(*this >= rhs);        }
+    bool operator!=(const LocationInfoFile& rhs) const { return !(*this == rhs);        }
 
 private:
-    std::array<LocationInfo, READ_MAX_ENTRY_SIZE> li_;
+    std::array<LocationInfoPtr, READ_MAX_SIZE> li_;
     std::size_t li_size_;
     std::ifstream infile_;
 };
@@ -79,6 +44,53 @@ private:
 /*****************************************************************************/
 //  Functions 
 /*****************************************************************************/
+LocationInfoFile::LocationInfoFile(const Filename& fn):
+    li_size_(0),
+    infile_(fn, std::ifstream::in)
+{
+    // read the array with maximum size  
+    LocationInfoPtr lip;
+    for(;!infile_.eof() && li_size_ < READ_MAX_SIZE; ++li_size_){
+        lip = LocationInfoPtr(new LocationInfo());
+        infile_ >> *lip;
+    }
+}
+
+
+bool LocationInfoFile::empty()
+{
+    return !infile_.is_open() && li_size_ == 0;
+}
+
+
+LocationInfoPtr LocationInfoFile::getEntry()
+{
+    if(empty()){
+        return nullptr;
+    }
+
+    // pop the min entry and size - 1
+    LocationInfoPtr min_loc = li_[READ_MAX_SIZE-li_size_];
+    li_[READ_MAX_SIZE-li_size_] = nullptr;
+    li_size_--;
+    
+    // if the array size is 0, read the file with max read size 
+    if(li_size_ == 0 && infile_.is_open()){
+        LocationInfoPtr lip;
+        for(;!infile_.eof() && li_size_ < READ_MAX_SIZE; ++li_size_){
+            lip = LocationInfoPtr(new LocationInfo());
+            infile_ >> *lip;
+        }
+        
+        if(infile_.eof()){
+            infile_.close();
+        }
+    }
+
+    return min_loc;
+}
+
+
 void esort_sort_file_task(const FilenameList& esfl)
 {
     std::size_t local_esfl_index;
@@ -89,11 +101,24 @@ void esort_sort_file_task(const FilenameList& esfl)
         }
 
         // read file
+        std::vector<LocationInfo> li_list;
+        const Filename& fn = esfl[local_esfl_index];
+        std::ifstream infile(fn, std::ifstream::in);
+        std::copy(std::istream_iterator<LocationInfo>(infile),
+                  std::istream_iterator<LocationInfo>(),
+                  std::back_inserter(li_list));
 
+        infile.close();
+    
         // quick sort
+        std::sort(li_list.begin(), li_list.end());
 
         // wirte file and close
-    
+        std::ofstream out_file(fn, std::ofstream::out);
+        std::copy(li_list.begin(),
+                  li_list.end(),
+                  std::ostream_iterator<LocationInfo>(out_file, "\n"));
+        out_file.close();
     }
 }
 
@@ -111,15 +136,75 @@ void esort_sort_file(const FilenameList& fn_list)
 void esort_merge_sort_files(const FilenameList& fn_list,
                             const Filename& esort_fn)
 {
+    constexpr std::size_t WRITE_MAX_SIZE = 100000;
 
+    // create LocationInfoFile list
+    std::vector<LocationInfoFile> lif_list;
+    for(auto fn: fn_list){
+        lif_list.push_back(std::move(LocationInfoFile(fn)));
+    }
+    // heapify the LocationInfoFile list  first
+    std::make_heap(lif_list.begin(), lif_list.end());
+
+    // open the merge file and create empty location infor list
+    std::ofstream esort_out(esort_fn, std::ofstream::out);
+    std::vector<LocationInfoPtr> li_list;
+
+    while(!lif_list.empty()){
+        // pop out the min element from the LocationInfoFile list
+        std::pop_heap(lif_list.begin(), lif_list.end());
+
+        LocationInfoFile& lif = lif_list[lif_list.size()-1];
+        LocationInfoPtr min_li= lif.getEntry();
+        if(min_li != nullptr){
+            li_list.push_back(min_li);
+        }
+        else{
+            std::cerr << "Error in find the min element" << std::endl;
+        }
+        
+        // if the min element entry is empty, remove the LocationInfoFile element 
+        if(lif.empty()){
+            lif_list.pop_back();
+        }
+
+        // pop in the LocationInfoFile entry into the list
+        std::push_heap(lif_list.begin(), lif_list.end());
+
+        // the merge array size is WRITE_MAX_SIZE, write to the merge file
+        // (Optional) print sepearate operator, if the x and y value of two
+        // continued element is not the same.
+        if(li_list.size() >= WRITE_MAX_SIZE){
+            for(auto& li: li_list){
+                esort_out << *li << "\n";
+            }
+            li_list.clear();
+        }
+
+    }
+
+    // if the LocationInfoFile list is not empty, write the remaining element
+    // to the merge file
+    if(li_list.size() > 0){
+        for(auto& li: li_list){
+            esort_out << *li << "\n";
+        }
+        li_list.clear();
+    }
+
+    // and close the merge file
+    esort_out.close();
 }
 
 void esort(std::shared_ptr<FilenameList> fn_list,
            const Filename esort_fn)
 {
+    // sort each file which contain partail subcommon subseqencs parallely.
     esort_sort_file(*fn_list);
 
+    // merge these sorted file into a file.
     esort_merge_sort_files(*fn_list, esort_fn);
 }
 
 }
+
