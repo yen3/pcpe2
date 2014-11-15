a754ad80bfd887d7a139e9794ad36843b923c5f1
yen3 <yen3rc@gmail.com>
2014-11-15 17:59:55 +0800

    ready to add esort test case

---
+++ src/main_compare/test/test_esort.cpp
@@ -0,0 +1,29 @@
+#if defined(__GTEST_PCPE__)
+#include "hash_table.h"
+#include "pcpe_util.h"
+#include "esort.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <gtest/gtest.h>
+
+namespace pcpe{
+
+extern void esort_sort_file_task(const FilenameList& esfl);
+extern void esort_sort_file(const FilenameList& fn_list);
+extern void esort_merge_sort_files(const FilenameList& fn_list,
+                                   const Filename& esort_fn);
+extern void esort(std::shared_ptr<FilenameList> fn_list,
+                  const Filename esort_fn);
+
+TEST(esort, esort_init){
+
+}
+
+}
+
+#endif /* __GTEST_PCPE__ */
