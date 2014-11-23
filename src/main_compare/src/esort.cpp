8ceb9994af57355feda76d2ec688392e198f43da
yen3 <yen3rc@gmail.com>
2014-11-23 09:05:02 +0800

    use clang-format with google style

--- src/main_compare/src/esort.cpp
+++ src/main_compare/src/esort.cpp
@@ -11,7 +11,7 @@
 #include "pcpe_util.h"
 #include "esort.h"
 
-namespace pcpe{
+namespace pcpe {
 
 /*****************************************************************************/
 // Global Variables
@@ -19,10 +19,9 @@ namespace pcpe{
 std::atomic_uint esfl_index(0);
 
 /*****************************************************************************/
-//  Functions 
+//  Functions
 /*****************************************************************************/
-void esort_sort_file(const Filename& fn)
-{
+void esort_sort_file(const Filename& fn) {
     static const std::size_t kBufferSize = 100000;
 
     // read file to li_list
@@ -36,13 +35,11 @@ void esort_sort_file(const Filename& fn)
     ComSubseqFileWriter::writeFile(fn, li_list, kBufferSize);
 }
 
-
-void esort_sort_file_task(const FilenameList& esfl)
-{
+void esort_sort_file_task(const FilenameList& esfl) {
     std::size_t local_esfl_index;
-    while(1){
+    while (1) {
         local_esfl_index = esfl_index.fetch_add(1);
-        if(local_esfl_index >= esfl.size()){
+        if (local_esfl_index >= esfl.size()) {
             break;
         }
 #if defined(__DEBUG__) && !defined(__GTEST_PCPE__)
@@ -55,27 +52,24 @@ void esort_sort_file_task(const FilenameList& esfl)
 
 void esort_sort_files(const FilenameList& fn_list) {
     std::vector<std::thread> tasks(std::thread::hardware_concurrency());
-    for(auto& task : tasks){
+    for (auto& task : tasks) {
         task = std::thread(esort_sort_file_task, fn_list);
     }
-    for(auto& task : tasks){
+    for (auto& task : tasks) {
         task.join();
     }
 }
 
-
 void esort_merge_sort_files(const FilenameList& fn_list,
                             const Filename& esort_fn,
-                            const std::size_t kBufferSize = 100000)
-{
-
+                            const std::size_t kBufferSize = 100000) {
 #if defined(__DEBUG__) && !defined(__GTEST_PCPE__)
     std::size_t write_count = 0;
 #endif
 
     // create read_file list
     std::vector<ComSubseqFileReader> csfr_list;
-    for(auto fn: fn_list){
+    for (auto fn : fn_list) {
         csfr_list.push_back(ComSubseqFileReader(fn, kBufferSize));
     }
 
@@ -85,7 +79,7 @@ void esort_merge_sort_files(const FilenameList& fn_list,
 
     // open the merge output file and create empty location infor list
     ComSubseqFileWriter esort_out(esort_fn, kBufferSize);
-    while(!csfr_list.empty()){
+    while (!csfr_list.empty()) {
         // get the min element from these files
         ComSubseq seq;
         std::pop_heap(csfr_list.begin(), csfr_list.end(),
@@ -95,19 +89,19 @@ void esort_merge_sort_files(const FilenameList& fn_list,
         // write the min element to the merge output file
         esort_out.writeSeq(seq);
 
-#if defined(__DEBUG__)  && !defined(__GTEST_PCPE__)
-        if(write_count++ % 100000 == 0){
-            std::cout << __FILE__ << " " << __LINE__ << ": " << "write "
-                      << write_count << " " << csfr_list.size() << std::endl;
+#if defined(__DEBUG__) && !defined(__GTEST_PCPE__)
+        if (write_count++ % 100000 == 0) {
+            std::cout << __FILE__ << " " << __LINE__ << ": "
+                      << "write " << write_count << " " << csfr_list.size()
+                      << std::endl;
         }
 #endif
 
         // if the reading state of the file is eof, the list would remove the
-        // file. Otherwise it push heap the file 
-        if(csfr_list.back().eof()){
+        // file. Otherwise it push heap the file
+        if (csfr_list.back().eof()) {
             csfr_list.pop_back();
-        }
-        else{
+        } else {
             std::push_heap(csfr_list.begin(), csfr_list.end(),
                            ComSubseqFileReader::esortMergeCompare);
         }
@@ -117,10 +111,7 @@ void esort_merge_sort_files(const FilenameList& fn_list,
     esort_out.close();
 }
 
-
-void esort(std::shared_ptr<FilenameList> fn_list,
-           const Filename esort_fn)
-{
+void esort(std::shared_ptr<FilenameList> fn_list, const Filename esort_fn) {
     // sort each file which contain partail subcommon subseqencs parallely.
     std::cout << "esrot parallel - start" << std::endl;
     esort_sort_files(*fn_list);
@@ -131,7 +122,4 @@ void esort(std::shared_ptr<FilenameList> fn_list,
     esort_merge_sort_files(*fn_list, esort_fn);
     std::cout << "esrot merge files - end" << std::endl;
 }
-
-
 }
-
