158cbe390f3b2a71a84738a953e6eb033fbee021
yen3 <yen3rc@gmail.com>
2016-11-27 09:49:34 +0800

    Add an example. We have to compress the input file since Github has file size limitation with 100 Mbytes

--- example/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python ../src/read_fasta/read_fasta.py influenza.faa
-python ../src/read_fasta/read_fasta.py protein.fa
-
-../src/main_compare/build/main.out influenza_seq_css.txt protein_seq_css.txt result.txt
