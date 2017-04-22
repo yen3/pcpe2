f87f5b242b55678030257e94ad31c7588b4997fc
yen3 <yen3rc@gmail.com>
2017-04-22 20:26:56 +0800

    Add test for GetNumberStep function

--- src/max_subseq/test/src/test_simple_task.cc
+++ src/max_subseq/test/src/test_simple_task.cc
@@ -24,8 +24,6 @@ class AssignIntTask {
 };
 
 TEST(simple_task, basic_usage) {
-  InitLogging(LoggingLevel::kDebug);
-
   std::vector<AssignIntTask*> tasks(100, nullptr);
 
   for (uint32_t i = 0; i < tasks.size(); ++i)
@@ -40,6 +38,50 @@ TEST(simple_task, basic_usage) {
     delete task;
     task = nullptr;
   }
+}
+
+TEST(simple_task, GetNumberSteps_empty) {
+  std::vector<std::size_t> steps;
+  GetNumberSteps(0, 10, steps);
+  ASSERT_EQ(steps.size(), 0);
+}
+
+TEST(simple_task, GetNumberSteps_regular) {
+  {
+    std::vector<std::size_t> steps;
+    GetNumberSteps(100, 10, steps);
+
+    std::vector<std::size_t> ans{0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    for (auto s : steps)
+      LOG_INFO() << s << std::endl;
+
+    ASSERT_EQ(steps.size(), ans.size());
+
+    for (std::size_t i = 0; i < ans.size(); ++i)
+      ASSERT_EQ(steps[i], ans[i]);
+  }
+
+  {
+    std::vector<std::size_t> steps;
+    GetNumberSteps(11, 10, steps);
+    std::vector<std::size_t> ans{0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    ASSERT_EQ(steps.size(), ans.size());
+
+    for (std::size_t i = 0; i < ans.size(); ++i)
+      ASSERT_EQ(steps[i], ans[i]);
+  }
+}
+
+TEST(simple_task, GetNumberSteps_num_less_step) {
+  std::vector<std::size_t> steps;
+  GetNumberSteps(7, 10, steps);
+  std::vector<std::size_t> ans{0, 1, 2, 3, 4, 5, 6, 7};
+
+  ASSERT_EQ(steps.size(), ans.size());
+
+  for (std::size_t i = 0; i < ans.size(); ++i)
+    ASSERT_EQ(steps[i], ans[i]);
 }
 
 } // namespace pcpe
