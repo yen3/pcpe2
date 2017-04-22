485e2f481e9565bec3ffc8dc5cd6115aa0fa6c83
yen3 <yen3rc@gmail.com>
2017-04-22 20:25:02 +0800

    Warnning when the task is empty.

--- src/max_subseq/inc/simple_task.h
+++ src/max_subseq/inc/simple_task.h
@@ -24,6 +24,24 @@ namespace pcpe {
 template<typename Task>
 void RunSimpleTasks(std::vector<Task*>& tasks);
 
+
+/**
+ * Generate step numbers in the range from 0 to n .
+ *
+ * Example:
+ *  Given n = 100, step_size = 10. The output is
+ *  {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+ *
+ * @param[in] n The end of the number
+ * @param[in] step_size the number of steps
+ * @param[out] steps The steps from 0 to n
+ *
+ * */
+void GetNumberSteps(const std::size_t n,
+                    const std::size_t step_size,
+                    std::vector<std::size_t>& steps);
+
+
 template<typename Task>
 void RunSimpleTasksInternal(
     std::vector<Task*>& tasks,
@@ -39,6 +57,13 @@ void RunSimpleTasksInternal(
 
   while ((curr_index = curr_max_index.fetch_add(1)) < tasks_size) {
     Task* task = tasks[curr_index];
+
+    if (task == nullptr) {
+      LOG_WARNING() << "The job " << curr_index << " is empty."
+                    << std::endl;
+      continue;
+    }
+
     task->exec();
   }
 }
