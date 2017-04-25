ddae58405cc0fbc3a1d360f58455cc1dc130b3ad
yen3 <yen3rc@gmail.com>
2017-04-25 21:56:14 +0800

    Implement a simple main

--- src/main.cxx
+++ src/main.cxx
@@ -3,16 +3,26 @@
 
 #include "logging.h"
 #include "small_seq_hash.h"
+#include "env.h"
 #include "pcpe_util.h"
 
-int main(int argc, char *argv[]) {
+void InitEnvironment(int argc, char* argv[]) {
   // Init the logging environment.
   pcpe::InitLogging(pcpe::LoggingLevel::kDebug);
 
+  // Create temp folder
+  const pcpe::FilePath& temp_folder = pcpe::gEnv.getTempFolderPath();
+  if (!pcpe::CheckFolderExists(temp_folder.c_str()))
+    pcpe::CreateFolder(temp_folder.c_str());
+
   if (argc <= 3) {
     LOG_ERROR() << "Need two input file." << std::endl;
-    return 1;
+    exit(1);
   }
+}
+
+int main(int argc, char *argv[]) {
+  InitEnvironment(argc, argv);
 
   std::vector<pcpe::FilePath> cs_filepaths;
   pcpe::FilePath xfilepath(argv[1]);
