be1d92940b575a33ac203485a952257dca4d833c
yen3 <yen3rc@gmail.com>
2017-04-25 16:33:19 +0800

    Fix bug: It's possible use log out of namespace pcpe

--- include/logging.h
+++ include/logging.h
@@ -32,13 +32,13 @@ void InitLogging(LoggingLevel default_level);
 void InitLogging(const std::string& filename, LoggingLevel default_level);
 void InitLogging(const char* filename, LoggingLevel default_level);
 
-#define LOG(level) LogMessage(level, __FILE__, __LINE__).stream()
+#define LOG(level) pcpe::LogMessage(level, __FILE__, __LINE__).stream()
 
-#define LOG_FATAL() LOG(LoggingLevel::kFatal)
-#define LOG_ERROR() LOG(LoggingLevel::kError)
-#define LOG_WARNING() LOG(LoggingLevel::kWarning)
-#define LOG_INFO() LOG(LoggingLevel::kInfo)
-#define LOG_DEBUG() LOG(LoggingLevel::kDebug)
+#define LOG_FATAL()   LOG(pcpe::LoggingLevel::kFatal)
+#define LOG_ERROR()   LOG(pcpe::LoggingLevel::kError)
+#define LOG_WARNING() LOG(pcpe::LoggingLevel::kWarning)
+#define LOG_INFO()    LOG(pcpe::LoggingLevel::kInfo)
+#define LOG_DEBUG()   LOG(pcpe::LoggingLevel::kDebug)
 
 } // namespace pcpe
 
