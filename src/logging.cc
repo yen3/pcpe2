5f2f207b26a73628f795fecd3f3e914500bf225e
yen3 <yen3rc@gmail.com>
2017-04-26 15:34:32 +0800

    Refine coding style.

--- src/logging.cc
+++ src/logging.cc
@@ -8,36 +8,36 @@ namespace pcpe {
 
 class LogObject {
  public:
-    virtual ~LogObject() {}
-    virtual std::ostream& stream() = 0;
-    virtual void close() = 0;
+  virtual ~LogObject() {}
+  virtual std::ostream& stream() = 0;
+  virtual void close() = 0;
 };
 
 class LogStdErrObject : public LogObject {
  public:
-     LogStdErrObject(): LogObject() {}
-     virtual ~LogStdErrObject() {}
-     virtual std::ostream& stream() {  return std::cerr; }
-     virtual void close() {}
+  LogStdErrObject(): LogObject() {}
+  virtual ~LogStdErrObject() {}
+  virtual std::ostream& stream() {  return std::cerr; }
+  virtual void close() {}
 };
 
 class LogFileObject : public LogObject {
  public:
-    explicit LogFileObject(const char* filename):
-        LogObject(), out_(filename, std::ofstream::out), filename_(filename) {
-    }
-    virtual ~LogFileObject() {
-        close();
-    }
-    virtual std::ostream& stream() { return out_; }
-    virtual void close() {
-      out_.flush();
-      out_.close();
-    }
+  explicit LogFileObject(const char* filename):
+    LogObject(), out_(filename, std::ofstream::out), filename_(filename) {
+  }
+  virtual ~LogFileObject() {
+    close();
+  }
+  virtual std::ostream& stream() { return out_; }
+  virtual void close() {
+    out_.flush();
+    out_.close();
+  }
 
  private:
-    std::ofstream out_;
-    const std::string filename_;
+  std::ofstream out_;
+  const std::string filename_;
 };
 
 class LogRecorder {
@@ -46,12 +46,13 @@ class LogRecorder {
     out_obj_(nullptr), filter_level_(LoggingLevel::kInfo), mutex_() {
   }
 
-  void initStdErr(LoggingLevel level=LoggingLevel::kInfo) {
+  void initStdErr(LoggingLevel level = LoggingLevel::kInfo) {
     LogObject* obj = new LogStdErrObject();
     initInternal(obj, level);
   }
 
-  void initFile(const char* filename, LoggingLevel level=LoggingLevel::kInfo) {
+  void initFile(const char* filename,
+      LoggingLevel level = LoggingLevel::kInfo) {
     LogObject* obj = new LogFileObject(filename);
     initInternal(obj, level);
   }
@@ -73,9 +74,9 @@ class LogRecorder {
  private:
   void initInternal(LogObject* out_obj, LoggingLevel level) {
     if (out_obj_ != nullptr) {
-        out_obj_->close();
-        delete out_obj_;
-        out_obj_ = nullptr;
+      out_obj_->close();
+      delete out_obj_;
+      out_obj_ = nullptr;
     }
 
     out_obj_ = out_obj;
@@ -92,26 +93,26 @@ namespace {
 LogRecorder gLogRecorder;
 
 void WriteLog(LoggingLevel level,
-                     const char* filename,
-                     uint32_t lineno,
-                     const char* msg) {
-    gLogRecorder.record(level, filename, lineno, msg);
+              const char* filename,
+              uint32_t lineno,
+              const char* msg) {
+  gLogRecorder.record(level, filename, lineno, msg);
 }
 
 } // namespace
 
 void InitLogging(LoggingLevel default_level) {
-    gLogRecorder.initStdErr(default_level);
+  gLogRecorder.initStdErr(default_level);
 }
 
 void InitLogging(const std::string& filename,
                  LoggingLevel default_level) {
-    gLogRecorder.initFile(filename.c_str(), default_level);
+  gLogRecorder.initFile(filename.c_str(), default_level);
 }
 
 void InitLogging(const char* filename,
                  LoggingLevel default_level) {
-    gLogRecorder.initFile(filename, default_level);
+  gLogRecorder.initFile(filename, default_level);
 }
 
 LogMessage::LogMessage(LoggingLevel level,
@@ -121,24 +122,24 @@ LogMessage::LogMessage(LoggingLevel level,
 }
 
 LogMessage::~LogMessage() {
-    WriteLog(level_, filename_.c_str(), line_, out_.str().c_str());
+  WriteLog(level_, filename_.c_str(), line_, out_.str().c_str());
 }
 
 std::ostringstream& LogMessage::stream() {
-    return out_;
+  return out_;
 }
 
 void LogRecorder::record(LoggingLevel level,
                          const char* filename,
                          uint32_t lineno,
                          const char* msg) {
-    if (level > filter_level_)
-      return;
+  if (level > filter_level_)
+    return;
 
-    if (out_obj_ != nullptr) {
-      std::lock_guard<std::mutex> lock(mutex_);
-      out_obj_->stream() << "[" << filename << ":" << lineno << "]: " << msg;
-    }
+  if (out_obj_ != nullptr) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    out_obj_->stream() << "[" << filename << ":" << lineno << "]: " << msg;
+  }
 }
 
 } // namespace pcpe
